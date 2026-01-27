b6aea5863120da58f9d3ad1691fe9b5232938810
sohyeoklim <113007267+sohyeoklim@users.noreply.github.com>
2026-01-27 16:00:04 +0900

    test2에용

--- src/main/java/luckydrop/demo/ZZGD.java
+++ src/main/java/luckydrop/demo/ZZGD.java
@@ -4,6 +4,6 @@ public class ZZGD {
 
     public static void main(String[] args) {
         String aa = "fuckYou";
-        System.out.println(aa);
+        System.out.println("zzgd?" + aa);
     }
 }
