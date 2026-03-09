2bd1d9098a61f52905cc165ecb4bb72ec2557464
sohyeoklim <113007267+sohyeoklim@users.noreply.github.com>
2026-03-09 14:58:59 +0900

    D프론트 작업으로 인한esponse 쉉

--- src/main/java/luckydrop/demo/draw/controller/DrawQueryController.java
+++ src/main/java/luckydrop/demo/draw/controller/DrawQueryController.java
@@ -3,13 +3,12 @@ package luckydrop.demo.draw.controller;
 import lombok.RequiredArgsConstructor;
 import luckydrop.demo.common.member.CustomUserPrincipal;
 import luckydrop.demo.draw.dto.response.DrawDetailResponse;
-import luckydrop.demo.draw.dto.response.DrawSummaryResponse;
+import luckydrop.demo.draw.dto.response.DrawCardResponse;
 import luckydrop.demo.draw.dto.response.HotBannerResponse;
 import luckydrop.demo.draw.enums.DrawSort;
 import luckydrop.demo.draw.enums.DrawTab;
 import luckydrop.demo.draw.service.DrawQueryService;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +21,7 @@ public class DrawQueryController {
     private final DrawQueryService drawQueryService;
 
     @GetMapping
-    public ResponseEntity<Page<DrawSummaryResponse>> getDraws(
+    public ResponseEntity<Page<DrawCardResponse>> getDraws(
             @AuthenticationPrincipal CustomUserPrincipal principal,
             @RequestParam(defaultValue = "ALL") DrawTab tab,
             @RequestParam(required = false) DrawSort sort,
